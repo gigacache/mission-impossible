d3f09f2e7bd5bec7dc8ab65bc03746406dd732e0
gigacache <dmcgillz@icloud.com>
2024-01-30 14:34:05 +0000

    updated unit tests and ci

--- test/Service/SorterTest.php
+++ test/Service/SorterTest.php
@@ -7,4 +7,12 @@ use Mission\Impossible\Test\Service;
 
 class SorterTest extends Service
 {
+
+    public function testGetMissionByEvent(): void
+    {
+        $this->set('all');
+        $this->sorter->sortMissionCollection('event', 'application_item_datalake[saveItem]');
+        $test = $this->missionCollection;
+        $this->assertEquals([],[]);
+    }
 }
